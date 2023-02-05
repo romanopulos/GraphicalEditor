a650cbb4e7d514ab23af8af96f7c0dfbeaca372a
Roman Yeromenko <Yeromenko.Roman@halliburton.com>
2023-02-05 21:09:16 +0200

    Add project files.

---
+++ GraphicalEditorForm/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace GraphicalEditorForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBoxPaint = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPaint)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1282, 125);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 701);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1282, 52);
+            this.panel2.TabIndex = 2;
+            // 
+            // pictureBoxPaint
+            // 
+            this.pictureBoxPaint.BackColor = System.Drawing.Color.White;
+            this.pictureBoxPaint.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxPaint.Location = new System.Drawing.Point(0, 125);
+            this.pictureBoxPaint.Name = "pictureBoxPaint";
+            this.pictureBoxPaint.Size = new System.Drawing.Size(1282, 576);
+            this.pictureBoxPaint.TabIndex = 3;
+            this.pictureBoxPaint.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1282, 753);
+            this.Controls.Add(this.pictureBoxPaint);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alternative Paint";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPaint)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBoxPaint;
+    }
+}
