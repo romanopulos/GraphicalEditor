ba2df50ecc628d0a19350b5304279a7f10999bf1
Roman Yeromenko <Yeromenko.Roman@halliburton.com>
2023-02-09 22:04:40 +0200

    Initial commit _1

--- GraphicalEditorForm/Form1.Designer.cs
+++ GraphicalEditorForm/Form1.Designer.cs
@@ -29,19 +29,34 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.picColor = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBoxPaint = new System.Windows.Forms.PictureBox();
+            this.picColor2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPaint)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.picColor2);
+            this.panel1.Controls.Add(this.picColor);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1282, 125);
             this.panel1.TabIndex = 1;
+            // 
+            // picColor
+            // 
+            this.picColor.BackColor = System.Drawing.Color.White;
+            this.picColor.Font = new System.Drawing.Font("Times New Roman", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.picColor.Location = new System.Drawing.Point(26, 24);
+            this.picColor.Name = "picColor";
+            this.picColor.Size = new System.Drawing.Size(32, 35);
+            this.picColor.TabIndex = 0;
+            this.picColor.UseVisualStyleBackColor = false;
             // 
             // panel2
             // 
@@ -56,24 +71,38 @@
             // 
             this.pictureBoxPaint.BackColor = System.Drawing.Color.White;
             this.pictureBoxPaint.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBoxPaint.Location = new System.Drawing.Point(0, 125);
+            this.pictureBoxPaint.Location = new System.Drawing.Point(0, 0);
             this.pictureBoxPaint.Name = "pictureBoxPaint";
-            this.pictureBoxPaint.Size = new System.Drawing.Size(1282, 576);
+            this.pictureBoxPaint.Size = new System.Drawing.Size(1282, 753);
             this.pictureBoxPaint.TabIndex = 3;
             this.pictureBoxPaint.TabStop = false;
+            this.pictureBoxPaint.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxPaint_MouseDown);
+            this.pictureBoxPaint.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxPaint_MouseMove);
+            this.pictureBoxPaint.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBoxPaint_MouseUp);
+            // 
+            // picColor2
+            // 
+            this.picColor2.BackColor = System.Drawing.Color.White;
+            this.picColor2.Font = new System.Drawing.Font("Times New Roman", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.picColor2.Location = new System.Drawing.Point(64, 24);
+            this.picColor2.Name = "picColor2";
+            this.picColor2.Size = new System.Drawing.Size(32, 35);
+            this.picColor2.TabIndex = 1;
+            this.picColor2.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1282, 753);
-            this.Controls.Add(this.pictureBoxPaint);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBoxPaint);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Alternative Paint";
+            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPaint)).EndInit();
             this.ResumeLayout(false);
 
@@ -84,5 +113,7 @@
         private Panel panel1;
         private Panel panel2;
         private PictureBox pictureBoxPaint;
+        private Button picColor;
+        private Button picColor2;
     }
 }
